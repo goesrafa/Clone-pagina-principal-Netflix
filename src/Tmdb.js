58991f3df77706be3d31c096d8d48d99de853b39
goesrafa <rafa.goes.2012@gmail.com>
2021-10-18 17:31:44 -0300

    Projeto inicial

---
+++ src/Tmdb.js
@@ -0,0 +1,17 @@
+const API_KEY = '4ba747990f613ab6a3e57f77fa378b21';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+/**
+ * filmes originais
+ * recomendados (treading)
+ * em alta (top rated)
+ * ação
+ * comédia
+ * terror
+ * romance
+ * documentario
+ */
+
+export default {
+    
+}
